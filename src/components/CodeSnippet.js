6e326adeb7d0b5e5e3fe8a10f7bff836ee8d53ea
mustafa <mustafa.sadriwala@utdallas.edu>
2020-11-16 10:59:43 -0600

    added wrapper to code and sped up typist

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -18,47 +18,66 @@ const Code = ({ children, type, nospace }) => {
     )
 }
 
+const CodeWrapper = ({ children, isTypingDone }) => {
+    if (isTypingDone) {
+        return (
+            <Box elevation={"medium"} margin={{horizontal: "medium"}} round="small">
+                {children}
+            </Box>
+        )
+    } else {
+        return (
+            <Box elevation={"none"} margin={{horizontal: "medium"}}>
+                {children}
+            </Box>
+        )
+    }
+}
+
 export default function CodeSnippet() {
+    const [isTypingDone, setIsTypingDone] = React.useState(false);
     return (
-        <Typist cursor={{ show: false }}>
-            <Box margin={{ left: "medium" }}>
-                <div>
-                    <Code type="keyword">const club</Code><Code type="variable">GWC</Code><Code>{"= () => {"}</Code>
-                </div>
-                <Tab>
+        <CodeWrapper isTypingDone={isTypingDone}>
+            <Typist avgTypingDelay={45} cursor={{ show: false }} onTypingDone={() => { setIsTypingDone(true) }}>
+                <Box margin="medium">
                     <div>
-                        <Code type="keyword">if</Code><Code>{"( "}</Code><Code type="function" nospace>viewing</Code>
-                        <Code>{"("}</Code><Code type="variable" nospace>you</Code><Code>{") == "}</Code>
-                        <Code type="keyword" nospace>this</Code><Code nospace>{"."}</Code><Code type="variable">page</Code>
-                        <Code>{") {"}</Code>
+                        <Code type="keyword">const club</Code><Code type="variable">GWC</Code><Code>{"= () => {"}</Code>
                     </div>
                     <Tab>
                         <div>
-                            <Code type="function" nospace>print</Code><Code>{"("}</Code>
-                            <Code type="string" nospace>"Welcome to UTD Girls Who Code!"</Code><Code>{");"}</Code>
+                            <Code type="keyword">if</Code><Code>{"("}</Code><Code type="function" nospace>viewing</Code>
+                            <Code nospace>{"("}</Code><Code type="variable" nospace>you</Code><Code>{") == "}</Code>
+                            <Code type="keyword" nospace>this</Code><Code nospace>{"."}</Code><Code type="variable">page</Code>
+                            <Code>{") {"}</Code>
+                        </div>
+                        <Tab>
+                            <div>
+                                <Code type="function" nospace>print</Code><Code nospace>{"("}</Code>
+                                <Code type="string" nospace>"Welcome to UTD Girls Who Code!"</Code><Code>{");"}</Code>
+                            </div>
+                        </Tab>
+                        <Code>{"}"}</Code>
+                        <br />
+                        <div>
+                            <Code type="keyword">let</Code><Code type="variable">goals</Code><Code>{"= {"}</Code>
+                        </div>
+                        <Tab>
+                            <div>
+                                <Code type="variable" nospace>"one"</Code><Code>{": "}</Code><Code type="function" nospace>build_sisterhood()</Code><Code>{","}</Code><br />
+                                <Code type="variable" nospace>"two"</Code><Code>{": "}</Code><Code type="function" nospace>empower_womxn()</Code><Code>{","}</Code><br />
+                                <Code type="variable" nospace>"three"</Code><Code>{": "}</Code><Code type="function" nospace>learn_technical_skills()</Code><Code>{","}</Code>
+                            </div>
+                        </Tab>
+                        <Code>{"};"}</Code>
+                        <br />
+                        <div>
+                            <Code type="keyword">return</Code><Code type="variable" nospace>goals</Code><Code>{";"}</Code>
                         </div>
                     </Tab>
                     <Code>{"}"}</Code>
-                    <br />
-                    <div>
-                        <Code type="keyword">let</Code><Code type="variable">goals</Code><Code>{"= {"}</Code>
-                    </div>
-                    <Tab>
-                        <div>
-                            <Code type="variable" nospace>"one"</Code><Code>{": "}</Code><Code type="function" nospace>build_sisterhood()</Code><Code>{","}</Code><br />
-                            <Code type="variable" nospace>"two"</Code><Code>{": "}</Code><Code type="function" nospace>empower_womxn()</Code><Code>{","}</Code><br />
-                            <Code type="variable" nospace>"three"</Code><Code>{": "}</Code><Code type="function" nospace>learn_technical_skills()</Code><Code>{","}</Code>
-                        </div>
-                    </Tab>
-                    <Code>{"};"}</Code>
-                    <br />
-                    <div>
-                        <Code type="keyword">return</Code><Code type="variable" nospace>goals</Code><Code>{";"}</Code>
-                    </div>
-                </Tab>
-                <Code>{"}"}</Code>
-            </Box>
-        </Typist>
+                </Box>
+            </Typist>
+        </CodeWrapper>
     )
     // const club GWC = () => {
     //     if (viewing(you) == this.page) {
