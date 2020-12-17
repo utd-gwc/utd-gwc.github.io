ba7f8ba37bc70ac41218dd0a87974a86f548d0e5
mustafa <mustafa.sadriwala@utdallas.edu>
2020-12-17 13:36:31 -0600

    make about mobile-friendly

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,11 +3,10 @@ import { Box, Heading, Paragraph, Button } from 'grommet'
 import FadeInComponent from './FadeInComponent';
 
 export default function AboutSection(props) {
-
     return (
         <Box margin="medium" fill="horizontal">
             <FadeInComponent>
-                <Box fill="horizontal" align="start">
+                <Box fill="horizontal" align="start" margin={{ bottom: 'large' }}>
                     <Box background="neutral-1" pad="medium" round="small">
                         <Heading level={3} size="medium" margin={{ top: "none" }} textAlign="end">
                             @ UTD
@@ -23,16 +22,16 @@ export default function AboutSection(props) {
                     <Box background="card-background" pad="medium" round="small">
                         <Heading level={3} size="medium" margin={{ top: "none" }}>
                             Internationally
-                    </Heading>
+                        </Heading>
                         <Paragraph size="medium" textAlign="end">
                             Girls Who Code is an international non-profit organization working to close the gender gap in technology and lead the movement to inspire, educate and equip young women with the computing skills needed to pursue 21st century opportunities. Since launching in the United States in 2012, Girls Who Code has reached 185,000 girls through its programs (clubs, campuses, summer immersion programs, College Loops) and over 100 million people through campaigns, advocacy work, and New York Times best-selling series!
-                    </Paragraph>
+                        </Paragraph>
                     </Box>
                 </Box>
                 <Box fill="horizontal" align="center" margin={{ bottom: "none" }}>
                     <Heading level={4}>
-                        Learn more about GWC from the founders!
-                </Heading>
+                        Learn more about GWC from its founders!
+                    </Heading>
                     <Button size="medium" secondary outlined label="Learn More" href="https://girlswhocode.com/" />
                 </Box>
             </FadeInComponent>
